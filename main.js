c758cf8fc74bff9c1cf0e9cb7aa0bbdb6cc31a9a
Arjun Nair <arjunnair.mec@gmail.com>
2018-07-15 04:47:08 +0530

    Add Presets; GIF

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
-// Number of particles
-var N_PARTICLES = 100;
-// If the particles should be connected
-var P_CONNECTED = true;
-// Threshold for establishing connection
-var CONN_THRESH = 100;
-
 var windowWidth = window.innerWidth,
-    windowHeight = window.innerHeight;
+    windowHeight = window.innerHeight,
+    // Number of particles
+    N_PARTICLES = 100,
+    // If the particles should be connected
+    P_CONNECTED = true,
+    // Threshold for establishing connection
+    CONN_THRESH = 100,
+    // Particle constants
+    P_MAX_VEL = 1,
+    P_MAX_SIZE = 10,
+    P_MIN_SIZE = 3,
+    P_MIN_AGE = 50,
+    // If the color should remain fixed
+    P_CONST_COLOR_MODE = false,
+    P_CONST_COLORR = 0,
+    P_CONST_COLORG = 0,
+    P_CONST_COLORB = 0,
+    P_MAX_LIFE = 100,
+    BACKGROUND = [0, 0, 0, 200];
+
+var NUM_PRESETS = 3;
+// Sets preset values, set to 0 to disable presetting
+var PresetValue = 1;
+setPresets(PresetValue);
 
 var ParticleObject = function() {
-    this.MAX_VEL = 1;
-    this.MAX_SIZE = 10;
-    this.MIN_SIZE = 3;
-    this.MIN_AGE = 50;
-    this.MAX_LIFE = 100;
+    this.MAX_VEL = P_MAX_VEL;
+    this.MAX_SIZE = P_MAX_SIZE;
+    this.MIN_SIZE = P_MIN_SIZE;
+    this.MIN_AGE = P_MIN_AGE;
+    this.MAX_LIFE = P_MAX_LIFE;
+    this.CONST_COLOR_MODE = P_CONST_COLOR_MODE;
+    this.CONST_COLORR = P_CONST_COLORR;
+    this.CONST_COLORG = P_CONST_COLORG;
+    this.CONST_COLORB = P_CONST_COLORB;
     // FADE_TIME: fading in and out takes this percent of its life
     this.FADE_TIME = 0.2;
+    // Keeps track of current Preset
+    this.presetValue = PresetValue;
 
     // Always call init first
     this.init = function() {
+        if(PresetValue != this.presetValue) {
+            this.resetPresets();
+        }
         this.life = random(50, 300);
         this.size = random(this.MIN_SIZE, this.MAX_SIZE);
         this.xx = random(windowWidth);
@@ -26,9 +51,15 @@ var ParticleObject = function() {
         this.vx = random(-(this.MAX_VEL), this.MAX_VEL);
         this.vy = random(-(this.MAX_VEL), this.MAX_VEL);
         this.age = 0;
-        this.colorr = random(255);
-        this.colorg = random(255);
-        this.colorb = random(255);
+        if(this.CONST_COLOR_MODE) {
+            this.colorr = this.CONST_COLORR;
+            this.colorg = this.CONST_COLORG;
+            this.colorb = this.CONST_COLORB;
+        } else {
+            this.colorr = random(255);
+            this.colorg = random(255);
+            this.colorb = random(255);
+        }
         this.alpha = random(100);
         this.viewAlpha = this.alpha;
         // fadeAlpha: Change to alpha value when fading
@@ -79,10 +110,21 @@ var ParticleObject = function() {
         this.xx += this.vx;
         this.yy += this.vy;
     }
+
+    this.resetPresets = function() {
+        this.MAX_VEL = P_MAX_VEL;
+        this.MAX_SIZE = P_MAX_SIZE;
+        this.MIN_SIZE = P_MIN_SIZE;
+        this.MIN_AGE = P_MIN_AGE;
+        this.MAX_LIFE = P_MAX_LIFE;
+        this.CONST_COLOR_MODE = P_CONST_COLOR_MODE;
+        this.CONST_COLORR = P_CONST_COLORR;
+        this.CONST_COLORG = P_CONST_COLORG;
+        this.CONST_COLORB = P_CONST_COLORB;
+    }
 }
 
 var particles = [];
-
 
 var AGE_CONN_FADE_THRESH = 20; // Age to which fading happens
 var ALPHA_FINAL_ADJUST = 1; // A final adjustment to alpha value
@@ -135,7 +177,7 @@ function setup() {
 }
 
 function draw() {
-    background(0, 0, 0, 200);
+    background(BACKGROUND);
     for (var i = 0; i < N_PARTICLES; ++i) {
         particles[i].update();
         particles[i].draw();
@@ -146,7 +188,66 @@ function draw() {
     }
 }
 
+// Sets preset values
+function setPresets(num) {
+    switch(num) {
+        case 1:
+            N_PARTICLES = 1000;
+            P_CONNECTED = false;
+            CONN_THRESH = 100;
+            P_MAX_VEL = 1;
+            P_MAX_SIZE = 10;
+            P_MIN_SIZE = 3;
+            P_MIN_AGE = 50;
+            P_CONST_COLOR_MODE = false;
+            P_CONST_COLORR = 255;
+            P_CONST_COLORG = 255;
+            P_CONST_COLORB = 255;
+            P_MAX_LIFE = 100;
+            BACKGROUND = [0, 0, 0, 200];
+            break;
+        case 2:
+            N_PARTICLES = 100;
+            P_CONNECTED = true;
+            CONN_THRESH = 100;
+            P_MAX_VEL = 1;
+            P_MAX_SIZE = 10;
+            P_MIN_SIZE = 3;
+            P_MIN_AGE = 50;
+            P_CONST_COLOR_MODE = false;
+            P_CONST_COLORR = 0;
+            P_CONST_COLORG = 0;
+            P_CONST_COLORB = 0;
+            P_MAX_LIFE = 100;
+            BACKGROUND = [0, 0, 0, 200];
+            break;
+        case 3:
+            N_PARTICLES = 50;
+            P_CONNECTED = true;
+            CONN_THRESH = 100;
+            P_MAX_VEL = 1;
+            P_MAX_SIZE = 10;
+            P_MIN_SIZE = 3;
+            P_MIN_AGE = 50;
+            P_CONST_COLOR_MODE = true;
+            P_CONST_COLORR = 0;
+            P_CONST_COLORG = 0;
+            P_CONST_COLORB = 0;
+            P_MAX_LIFE = 100;
+            BACKGROUND = [255, 255, 255, 200];
+            break;
+    }
+}
+
+document.addEventListener("click", function() {
+    if (PresetValue) {
+        PresetValue = PresetValue % NUM_PRESETS + 1;
+        setPresets(PresetValue);
+    }
+});
+
+
 document.addEventListener("resize", function() {
-    innerWidth = window.innerWidth;
-    innerHeight = window.innerHeight;
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
 });
